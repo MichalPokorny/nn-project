84c7d3e15f94cfce0cc93b077d33c9ed9d11a7de
woitee <woitee@seznam.cz>
2015-12-11 12:46:51 +0100

    Filtering of users in matlab

--- neural-network/nn.m
+++ neural-network/nn.m
@@ -5,13 +5,23 @@ data = csvread(filename);
 
 %% Preprocess data so that inputs and outputs are columns
 
+% filter 0, 1 users
+data = data(data(:,1) ~= 0 & data(:,1) ~= 1,:);
+
+data = data(data(:,1) == 144 | data(:,1) == 145,:);
+
 targets = data(:,1)';
+
 uniq = unique(targets);
+freq = zeros(1, length(uniq));
 
 targetM = zeros(length(uniq), length(targets));
 for i = 1:length(uniq)
     for j = 1:length(targets)
-        targetM(i,j) = (targets(j) == uniq(i));
+        if (targets(j) == uniq(i))
+            targetM(i,j) = 1;
+            freq(i) = freq(i) + 1;
+        end
     end
 end
 
